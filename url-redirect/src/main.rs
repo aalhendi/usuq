fb5048a660cd61adb83f69ce90f6ae2f0478bf5b
aalhendi <abdulrazaq.alhendi@gmail.com>
2022-09-05 19:32:34 +0300

    feat: 404 handler

--- url-redirect/src/main.rs
+++ url-redirect/src/main.rs
@@ -1,5 +1,8 @@
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
 use axum::{extract::Extension, Router};
 use std::sync::Arc;
+use std::net::SocketAddr;
 
 mod prisma;
 mod routes;
@@ -16,8 +19,16 @@ async fn main() {
         .nest("/", routes::create_route())
         .layer(Extension(client));
 
-    axum::Server::bind(&"0.0.0.0:8081".parse().unwrap())
+    // fallback service for handling routes to unknown paths
+    let app = app.fallback(axum::routing::get(handle_404));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handle_404() -> impl IntoResponse{
+    (StatusCode::NOT_FOUND, "lol, not found.")
 }
